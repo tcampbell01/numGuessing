75c895292ce790a4f4c142a59366cf9426b99026
tcampbell01 <teresa.j.campbell@gmail.com>
2024-06-24 13:31:28 -0400

    initial commit

--- src/main/java/org/example/NumGuesser.java
+++ src/main/java/org/example/NumGuesser.java
@@ -1,28 +1,34 @@
+package org.example;
+
 import java.util.Random;
 import java.util.Scanner;
 
-public class ClassNameHere {
+public class NumGuesser {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Random random = new Random();
+        boolean playAgain = true;
 
-
-
-        int randomNumber = random.nextInt(10) + 1;
-        boolean correctChoice = false;
-
-        while (input.hasNext()) {
+        while (playAgain) {
+            int randomNumber = random.nextInt(10) + 1;
+            boolean correctChoice = false;
 
             while (!correctChoice) {
                 System.out.print("Enter num: ");
                 int num = input.nextInt();
-                if ( num == randomNumber ) {
+                if (num == randomNumber) {
                     correctChoice = true;
                     System.out.println("Congratulations!");
                 } else {
                     System.out.println("Sorry, try again.");
                 }
+            }
+
+            System.out.print("Do you want to play again? (yes/no): ");
+            String response = input.next();
+            if (!response.equalsIgnoreCase("yes")) {
+                playAgain = false;
             }
         }
 
